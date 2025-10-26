3b6ed139fe36f284d2e92cbe9f7d02bf65f7273c
Nayeli Belén Guerrero Gutierrez <nayeli.guerrero@utec.edu.pe>
2025-10-25 23:13:28 -0500

    Add files via upload

--- BuscarAlumno.py
+++ BuscarAlumno.py
@@ -1,20 +1,21 @@
 import boto3
 
 def lambda_handler(event, context):
-    # Entrada (json)
-    tenant_id = event['tenant_id']
-    alumno_id = event['alumno_id']
-    # Proceso
+    tenant_id = event['queryStringParameters']['tenant_id']
+    alumno_id = event['pathParameters']['alumno_id']
+
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('t_alumnos')
+
     response = table.get_item(
         Key={
             'tenant_id': tenant_id,
             'alumno_id': alumno_id
         }
     )
-    # Salida (json)
+
     return {
         'statusCode': 200,
         'response': response
     }
+
